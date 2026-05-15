b3524507271a5e7656b452ff90e6f708f4eaad8e
Michael <molatunji371@gmail.com>
2026-05-15 10:35:43 +0100

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-
-function Card(){
-    
+function Prop({day, time}) {
     return(
         <React.Fragment>
-
+            <div className="card flex flex-col items-center justify-center gap-1 px-4 py-1 bg-[#743A18] rounded-lg shadow-md  cursor-pointer">
+                 <h2 className="text-[10px] text-[#D19A17]">{day}</h2>
+                 <p className="text-[12px] text-white font-bold"  >{time}</p>
+            </div>
+          
         </React.Fragment>
     )
 }
 
-export default Card
+export default Prop
